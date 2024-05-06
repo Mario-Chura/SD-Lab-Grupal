9a5df5ecec363e56b0770b76d2000d1f9615c1b6
Oliver <omayta@unsa.edu.pe>
2024-05-05 20:19:12 -0500

    Comentarios agregados a la clase Cajera y MainRunnable

--- Lab01/Anexo1/Cajera.java
+++ Lab01/Anexo1/Cajera.java
@@ -1,32 +1,46 @@
 package Anexo1;
 public class Cajera {
-    private String nombre;
+    private String nombre;// Nombre de la cajera
+
+    // Constructor por defecto
     public Cajera() {
 
     }
+
+    // Constructor que inicializa el nombre de la cajera.
     public Cajera(String nombre) {
 
         this.nombre = nombre;
     }
+
+    // Método para obtener el nombre de la cajera.
     public String getNombre() {
         return nombre;
     }
+
+    // Método para establecer el nombre de la cajera.
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    // Método que simula el proceso de compra de un cliente por parte de la cajera.
     public void procesarCompra(Cliente cliente, long timeStamp) {
+        // Se imprime un mensaje indicando el inicio del proceso de compra del cliente.
         System.out.println("La cajera " + this.nombre + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + cliente.getNombre() +
         " EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
-        
+        // Se procesa cada producto en el carro de compra del cliente.
         for (int i = 0; i < cliente.getCarroCompra().length; i++) {
+            // Se simula el procesamiento del producto.
             this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            // Se imprime un mensaje indicando el procesamiento del producto.
             System.out.println("Procesado el producto " + (i + 1) + " ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
         }
+        // Se imprime un mensaje indicando la finalización del proceso de compra del cliente.
         System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " +
         cliente.getNombre() + " EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
     
     }
-    
+    // Método privado que simula la espera de cierta cantidad de segundos.
     private void esperarXsegundos(int segundos) {
         try {
             Thread.sleep(segundos * 1000);
--- Lab01/Anexo1/MainRunnable.java
+++ Lab01/Anexo1/MainRunnable.java
@@ -1,27 +1,34 @@
 package Anexo1;
 
 public class MainRunnable implements Runnable{
-    private Cliente cliente;
-    private Cajera cajera;
-    private long initialTime;
+    private Cliente cliente; // Cliente que se está atendiendo
+    private Cajera cajera; // Cajera que atiende al cliente
+    private long initialTime; // Tiempo inicial de referencia
     
+    // Constructor que inicializa los atributos de la clase.
     public MainRunnable (Cliente cliente, Cajera cajera, long initialTime){
         this.cajera = cajera;
         this.cliente = cliente;
         this.initialTime = initialTime;
     }
+
+    // Método principal que inicia la simulación de la atención de clientes por parte de las cajeras.
     public static void main(String[] args) {
+        // Creación de clientes y cajeras
         Cliente cliente1 = new Cliente("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
         Cliente cliente2 = new Cliente("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
         Cajera cajera1 = new Cajera("Cajera 1");
         Cajera cajera2 = new Cajera("Cajera 2");
         // Tiempo inicial de referencia
         long initialTime = System.currentTimeMillis();
+        // Creación de procesos para cada cliente y cajera, y ejecución en hilos separados
         Runnable proceso1 = new MainRunnable(cliente1, cajera1, initialTime);
         Runnable proceso2 = new MainRunnable(cliente2, cajera2, initialTime);
         new Thread(proceso1).start();
         new Thread(proceso2).start();
     }
+    
+    // Método que se ejecuta cuando se inicia el hilo. En este método se llama al método procesarCompra de la cajera para procesar la compra del cliente.
     @Override
     public void run() {
         this.cajera.procesarCompra(this.cliente, this.initialTime);
